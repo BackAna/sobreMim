f079dcad2c1c90c76ee264e9bc5d59a0ce263d19
ana-back <anabackmsb@gmail.com>
2023-12-14 17:13:20 -0300

    inserido icone de menu

--- javascript/header.js
+++ javascript/header.js
@@ -3,3 +3,19 @@ fetch('header.html')
 .then(data => {
     document.querySelector('body').insertAdjacentHTML('afterbegin', data);
 });
+
+function clickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
